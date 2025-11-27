8215b6b040e34d47c23a389e00c4579725b478ad
chamberlay <chamberlay.vega@istea.com.ar>
2025-11-27 16:57:31 -0300

    servicios en oferta con promesas

--- app.js
+++ app.js
@@ -1,77 +1,81 @@
-import { air_Table_Token, air_Table_Base_Id, table_Name} from './env.js';
+//importo el archivo env.js con las keys de Airtable
+import { air_Table_Token, air_Table_Base_Id} from './env.js';
 
-document.addEventListener("DOMContentLoaded", () => {
+// Configuración de Airtable
+const airTableToken = air_Table_Token;
+const airTableBaseId = air_Table_Base_Id;
 
-    //funcion para agregar productos a las secciones
-    function agregarProducto(clase, href, src, titulo, precio){
-        const productos = document.querySelector(clase);
-        if(!productos) return;
+//funcion para agregar productos a las secciones
+function agregarProducto(clase, href, src, titulo, precio){
+    const productos = document.querySelector(clase);
+    if(!productos) return;
 
-        const contenedor = document.createElement("div");
-        contenedor.className = "producto";
+    const contenedor = document.createElement("div");
+    contenedor.className = "producto";
 
-        const newAnchor = document.createElement("a");
-        newAnchor.href = href;
-        newAnchor.target = "_blank";
+    const newAnchor = document.createElement("a");
+    newAnchor.href = href;
+    //newAnchor.target = "_blank";
 
-        const newImg = document.createElement("img");
-        newImg.src = src;
-        newImg.width = 300;
-        newImg.height = 200;
+    const newImg = document.createElement("img");
+    newImg.src = src;
+    newImg.width = 300;
+    newImg.height = 200;
 
-        const newH3 = document.createElement("h3");
-        newH3.textContent = titulo;
+    const newH3 = document.createElement("h3");
+    newH3.textContent = titulo;
 
-        const newPrice = document.createElement("p");
-        newPrice.textContent = `$${precio}`;
-        newPrice.className = "precio";
+    const newPrice = document.createElement("p");
+    newPrice.textContent = `$${Number(precio).toLocaleString("es-AR")}`;
+    newPrice.className = "precio";
 
 
-        newAnchor.appendChild(newImg);
-        contenedor.appendChild(newAnchor);
-        contenedor.appendChild(newH3);
-        contenedor.appendChild(newPrice);
+    newAnchor.appendChild(newImg);
+    contenedor.appendChild(newAnchor);
+    contenedor.appendChild(newH3);
+    contenedor.appendChild(newPrice);
 
-        productos.appendChild(contenedor);
-    }
+    productos.appendChild(contenedor);
+}
 
-    // Configuración de Airtable
-    const airTableToken = air_Table_Token;
-    const airTableBaseId = air_Table_Base_Id;
-    const tableName = table_Name;
+// Función para obtener datos de Airtable
+export async function GetProductosFromAirTable(tableName) {
     const airTableUrl = `https://api.airtable.com/v0/${airTableBaseId}/${tableName}`;
+    try {
+        const respuesta = await fetch(airTableUrl, {
+            headers: {
+                Authorization: `Bearer ${airTableToken}`,
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await respuesta.json();
 
-    // Función para obtener datos de Airtable
-    async function GetProductosFromAirTable() {
-        try {
-            // no me esta cargando los productos en GitHub Pages
-            console.log("Llamando a Airtable en:", airTableUrl);
+        data.records.forEach(record => {
+            const fields = record.fields;
+            agregarProducto(
+                fields.Categoria,
+                fields.Url,
+                fields.Img,
+                fields.Nombre,
+                fields.Precio
+            );
 
-            const respuesta = await fetch(airTableUrl, {
-                headers: {
-                    Authorization: `Bearer ${airTableToken}`,
-                    "Content-Type": "application/json"
-                }
-            });
-            const data = await respuesta.json();
-            console.log("Productos de AirTable: ", data);
-
-            data.records.forEach(record => {
-                const fields = record.fields;
-
+            const valor_Oferta = fields.Oferta === "true";
+            if (valor_Oferta) {
                 agregarProducto(
-                    fields.Categoria,
+                    ".contenedorOfertas",
                     fields.Url,
                     fields.Img,
                     fields.Nombre,
                     fields.Precio
-                );
-            }); 
-        }
-        catch (error) {
-            console.error("Error al obtener los datos de AirTable: ", error);
-        }
-    }
+                    );
+            }
 
-    GetProductosFromAirTable();
-});
+        }); 
+    }
+    catch (error) {
+        console.error("Error al obtener los datos de AirTable: ", error);
+    }
+}
+
+GetProductosFromAirTable("Productos");
--- env.js
+++ env.js
@@ -1,3 +1,2 @@
 export const air_Table_Token = "pat7WIp6eyTQda8si.511f3239df29777613e6f749b59940f18d15d764f3da1b3a5bc4f4742cc61054";
 export const air_Table_Base_Id = "app2xHmIx8fMyiNyJ";
-export const table_Name = "Productos";
